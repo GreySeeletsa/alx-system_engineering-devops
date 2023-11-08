83a05a03355fd34be833a514b9ae80fdcce2260d
GreySeeletsa <seeletsagrey@gmail.com>
2023-11-08 03:16:22 -0800

    debug wordpress website

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Debug a web stack that uses LAMP stack
+exec { 'fix-wordpress':
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
+/var/www/html/wp-settings.php; service apache2 restart"',
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
